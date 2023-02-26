99dac15046722761cc4fe37ae5f0f8ea25fa593a
Deepak sakthi V K <117092860+Deepak484sakthi2004@users.noreply.github.com>
2023-02-26 20:06:18 +0530

    Add files via upload

---
+++ pages/2_Educational_details.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from PIL import Image
+import os
+
+
+st.subheader(":red[Schooling:]")
+#image =os.path.join(dir_of_interest, "images", "College.jpg")
+
+st.caption("St.Joseph\'s College Of Engineering,CHN  ")
+
+##image = Image.open('sc.jng')
+st.caption("Sri Chaitanya Techno Schools, BLR")
+
+
+
